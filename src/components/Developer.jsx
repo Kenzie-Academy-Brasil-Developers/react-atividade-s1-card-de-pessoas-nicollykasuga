f4bf94478a0e188c9b0c9781c208102f7101d289
Nicky <nicollyporfirio12@gmail.com>
2021-10-27 10:40:40 -0300

    Fixed: cllas name

--- src/components/Developer.jsx
+++ src/components/Developer.jsx
@@ -1,10 +1,10 @@
-import './style.css'
-export const Developer = ({name, age,country}) =>(
-    <div>
-        <ul>
-            <li className='lista'>Dev: {name}</li>
-            <li className='lista'>Idade: {age}</li>
-            <li className='lista'>País: {country}</li>
-        </ul>
-    </div>
+import "./style.css";
+export const Developer = ({ name, age, country }) => (
+  <div>
+    <ul className="lista">
+      <li>Dev: {name}</li>
+      <li>Idade: {age}</li>
+      <li>País: {country}</li>
+    </ul>
+  </div>
 );
